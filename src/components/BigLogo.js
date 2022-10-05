0ca45009b99a076057a8aee1481ed1d773b39c9a
Rui Hu <hurui141@gmail.com>
2022-10-04 17:55:31 -0700

    Separate BigLogo & Login from index to components

--- src/components/BigLogo.js
+++ src/components/BigLogo.js
@@ -1,13 +1,29 @@
 import { SparklesIcon } from '@heroicons/react/24/solid';
+import { TableCellsIcon } from '@heroicons/react/24/solid';
+import { UserIcon } from '@heroicons/react/24/solid';
 
-export const BigLogo = ({ heading, subheading }) => {
+export const BigLogo = ({ heading, subheading, iconName }) => {
   return (
     <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-      <SparklesIcon
-        className='mx-auto h-12 w-auto'
-        alt={`${heading} ${subheading}`}
-        color='rgb(88,66,225)'
-      />
+      {iconName === 'table' ? (
+        <TableCellsIcon
+          className='mx-auto h-12 w-auto'
+          alt={`${heading} ${subheading}`}
+          color='rgb(88,66,225)'
+        />
+      ) : iconName === 'user' ? (
+        <UserIcon
+          className='mx-auto h-12 w-auto'
+          alt={`${heading} ${subheading}`}
+          color='rgb(88,66,225)'
+        />
+      ) : (
+        <SparklesIcon
+          className='mx-auto h-12 w-auto'
+          alt={`${heading} ${subheading}`}
+          color='rgb(88,66,225)'
+        />
+      )}
 
       <h2 className='mt-6 text-center font-bold tracking-tight'>
         <p className='text-3xl text-transparent bg-clip-text bg-gradient-to-br from-indigo-700 to-indigo-500'>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 
-export const Login = () => {
+export const Login = ({ message }) => {
   return (
     <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
       <div className='bg-white py-4 px-4 shadow sm:rounded-lg sm:px-10'>
@@ -11,7 +11,7 @@ export const Login = () => {
                 className='inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50'
                 onClick={() => signIn()}
               >
-                <span>Get Started</span>
+                <span>{message}</span>
               </button>
             </div>
           </div>
