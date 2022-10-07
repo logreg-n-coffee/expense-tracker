2596b13ee26b53af67c4ed6f1165b874ebe6aae5
Rui Hu <hurui141@gmail.com>
2022-10-06 17:03:48 -0700

    Improve api structure

--- pages/api/google-sheets/categories.js
+++ pages/api/google-sheets/categories.js
@@ -1,58 +1,76 @@
 // an api to interact with the Google Spreadsheet (through googleapis) on worksheet named 'categories'
 
-// google api node.js client 
+// google api node.js client
 import { google } from 'googleapis';
 
-// jwt token 
+// jwt token
 import { getToken } from 'next-auth/jwt';
 
 // load the client side self-generated secret from environment variable
 const secret = process.env.NEXTAUTH_SECRET;
 
 const CategoriesHandler = async (req, res) => {
-  // get token from the login session
-  const token = await getToken({ req, secret });
-
-  // console.log('======token from handler======\n', token);
-
-  if (!token) {
-    res.status(401);
+  switch (req.method) {
+    case 'GET':
+      return getCategories();
+    default:
+      return res
+        .status(405)
+        .end(
+          `Not allowed. Expected request method is GET, but received ${req.method}`
+        );
   }
 
-  // load in the variables to access the google api auth client
-  const clientId = process.env.GOOGLE_CLIENT_ID;
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-  const accessToken = token?.accessToken;
+  /**
+   * Get all categories from a specified googlesheet with sheetId
+   */
+  async function getCategories() {
+    // get token from the login session
+    const token = await getToken({ req, secret });
 
-  // https://github.com/googleapis/google-api-nodejs-client#readme
-  const oauth2Client = new google.auth.OAuth2({
-    clientId,
-    clientSecret,
-  });
+    // console.log('======token from handler======\n', token);
 
-  oauth2Client.setCredentials({
-    // property name is access_token but not accessToken
-    access_token: accessToken,
-  });
+    if (!token) {
+      res
+        .status(401)
+        .end(
+          'Not authorized. Expected a token from the login session, but received none.'
+        );
+    }
 
-  const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
+    // load in the variables to access the google api auth client
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    const accessToken = token?.accessToken;
 
-  const range = `${process.env.CATEGORIES_SHEET_NAME ?? 'categories'}!A2:B`;
-
-  try {
-    const response = await sheets.spreadsheets.values.get({
-      // request params
-      spreadsheetId: process.env.SHEET_ID,
-      range,
+    // https://github.com/googleapis/google-api-nodejs-client#readme
+    const oauth2Client = new google.auth.OAuth2({
+      clientId,
+      clientSecret,
     });
 
-    res.json(response.data);
-    // console.log(response.data);
+    oauth2Client.setCredentials({
+      // property name is access_token but not accessToken
+      access_token: accessToken,
+    });
 
-  } catch (err) {
-    throw new Error(err);
+    const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
+
+    const range = `${process.env.CATEGORIES_SHEET_NAME ?? 'categories'}!A2:B`;
+
+    try {
+      const response = await sheets.spreadsheets.values.get({
+        // request params
+        spreadsheetId: process.env.SHEET_ID,
+        range,
+      });
+
+      res.json(response.data);
+      console.log(response.data);
+    } catch (err) {
+      throw new Error(err);
+    }
   }
-
 };
 
 export default CategoriesHandler;
--- pages/api/google-sheets/expenses.js
+++ pages/api/google-sheets/expenses.js
@@ -7,78 +7,89 @@ import { getToken } from 'next-auth/jwt';
 const secret = process.env.NEXTAUTH_SECRET;
 
 const ExpensesHandler = async (req, res) => {
-    // As we are adding expenses, make sure the req.method is 'POST
-    if (req.method === 'POST') {
-      // get token from the login session
-      const token = await getToken({ req, secret });
+  // As we are adding expenses, make sure the req.method is 'POST'
+  switch (req.method) {
+    case 'POST':
+      return addExpense();
+    default:
+      return res
+        .status(405)
+        .end(
+          `Not allowed. Expected request method is POST, but received ${req.method}`
+        );
+  }
 
-      if (!token) {
-        res.status(401);
-      }
+  async function addExpense() {
+    // get token from the login session
+    const token = await getToken({ req, secret });
 
-      // destruct the request body
-      const { body } = req;
-
-      // process the data
-      const parsedBody = JSON.parse(body);
-
-      // prepare the data for POST request
-      const values = [
-        [
-          // cell values
-          parsedBody.date,
-          parsedBody.category,
-          parsedBody.amount,
-          parsedBody.description,
-        ],
-        // additional rows (if any)
-      ];
-
-      // load in the variables to access the google api auth client
-      const clientId = process.env.GOOGLE_CLIENT_ID;
-      const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-      const accessToken = token?.accessToken;
-
-      // https://github.com/googleapis/google-api-nodejs-client#readme
-      const oauth2Client = new google.auth.OAuth2({
-        clientId,
-        clientSecret,
-      });
-
-      oauth2Client.setCredentials({
-        access_token: accessToken,
-      });
-
-      const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
-
-      const range = `${process.env.EXPENSES_SHEET_NAME ?? 'expenses'}!A1:C1`;
-
-      const requestBody = { 
-        // desired properties to the request body
-        values,
-      };
-
-      // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
-      
-      try {
-        const response = await sheets.spreadsheets.values.append({
-          // request params
-          spreadsheetId: process.env.SHEET_ID,
-          range,
-          valueInputOption: 'USER_ENTERED',
-          requestBody,
-        });
-
-        res.json(response.data);
-        console.log(response.data);
-
-      } catch (err) {
-        throw new Error(err);
-      }
-
-    } else {
-        res.status(404);
+    if (!token) {
+      res
+        .status(401)
+        .end(
+          'Not authorized. Expected a token from the login session, but received none.'
+        );
     }
+
+    // destruct the request body
+    const { body } = req;
+
+    // process the data
+    const parsedBody = JSON.parse(body);
+
+    // prepare the data for POST request
+    const values = [
+      [
+        // cell values
+        parsedBody.date,
+        parsedBody.category,
+        parsedBody.amount,
+        parsedBody.description,
+      ],
+      // additional rows (if any)
+    ];
+
+    // load in the variables to access the google api auth client
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    const accessToken = token?.accessToken;
+
+    // https://github.com/googleapis/google-api-nodejs-client#readme
+    const oauth2Client = new google.auth.OAuth2({
+      clientId,
+      clientSecret,
+    });
+
+    oauth2Client.setCredentials({
+      access_token: accessToken,
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
+
+    const range = `${process.env.EXPENSES_SHEET_NAME ?? 'expenses'}!A1:C1`;
+
+    const requestBody = {
+      // desired properties to the request body
+      values,
+    };
+
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
+
+    try {
+      const response = await sheets.spreadsheets.values.append({
+        // request params
+        spreadsheetId: process.env.SHEET_ID,
+        range,
+        valueInputOption: 'USER_ENTERED',
+        requestBody,
+      });
+
+      res.json(response.data);
+      console.log(response.data);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 };
 
 export default ExpensesHandler;
