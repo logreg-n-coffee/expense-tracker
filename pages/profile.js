4405737c2a5b6304ab25189e89c4e34841c47b64
Rui Hu <hurui141@gmail.com>
2022-10-09 23:58:39 -0700

    Add key for table items

--- pages/profile.js
+++ pages/profile.js
@@ -36,7 +36,7 @@ const Profile = () => {
           <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
             <div className='px-4 py-5 sm:px-6'>
               <h3 className='text-lg font-medium leading-6 text-gray-900'>
-                {session?.user?.name ? `${session?.user?.name}'s` : '' } Profile
+                {session?.user?.name ? `${session?.user?.name}'s` : ''} Profile
               </h3>
               <Image
                 className='inline-block h-14 w-14 rounded-full mt-4'
@@ -50,18 +50,21 @@ const Profile = () => {
             <div className='border-t border-gray-200 px-4 py-5 sm:p-0'>
               <dl className='sm:divide-y sm:divide-gray-200'>
                 <div className='py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6'>
-                  {/* Print out the table */}
-                  {Object.keys(tableData).map((element) => (
-                    <>
-                      <div className='mt-3'>
-                        <dt className='text-sm font-medium text-gray-500'>
-                          {element}
-                        </dt>
-                        <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 break-words'>
-                          {tableData[element]}
-                        </dd>
-                      </div>
-                    </>
+                  {Object.keys(tableData).map((element, index) => (
+                    <div className='mt-3' key={index}>
+                      <dt
+                        className='text-sm font-medium text-gray-500'
+                        key={element}
+                      >
+                        {element}
+                      </dt>
+                      <dd
+                        className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 break-words'
+                        key={tableData[element]}
+                      >
+                        {tableData[element]}
+                      </dd>
+                    </div>
                   ))}
                 </div>
               </dl>
