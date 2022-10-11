326dfdc04f8121ccad398d6d38d5e353ba367364
Rui Hu <hurui141@gmail.com>
2022-10-10 17:53:15 -0700

    Add create spreadsheet with title & improve coding

--- pages/api/google-sheets/spreadsheets.js
+++ pages/api/google-sheets/spreadsheets.js
@@ -1,53 +1,19 @@
 // an api to interact with the spreadsheet files
 
 import { google } from 'googleapis';
-import { getToken } from 'next-auth/jwt';
+
+// import utility functions
+import { getNextAuthToken, authenticate } from '../../../src/server-side/utils';
 
 // The requested scope is https://www.googleapis.com/auth/drive.file
 // The app can only See, edit, create, and delete only the specific Google Drive files you use with this app
-
-// helper functions
-async function getNextAuthToken(req) {
-  // get the next-auth secret
-  const secret = process.env.NEXTAUTH_SECRET;
-
-  // get the access_token from next-auth
-  const token = await getToken({ req, secret });
-
-  if (!token) {
-    res
-      .status(401)
-      .end(
-        'Not authorized. Expected a token from the login session, but received none.'
-      );
-  }
-  return token;
-}
-
-async function authenticate(token) {
-  // authenticate with google oauth client
-  const clientId = process.env.GOOGLE_CLIENT_ID;
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-  const accessToken = token?.accessToken;
-
-  const oauth2Client = new google.auth.OAuth2({
-    clientId,
-    clientSecret,
-  });
-
-  oauth2Client.setCredentials({
-    access_token: accessToken,
-  });
-
-  return oauth2Client;
-}
 
 const SpreadsheetsHandler = async (req, res) => {
   switch (req.method) {
     case 'GET':
       return await getSpreadsheetIdsInDriveRoot();
     case 'POST':
-      return await createABlankSheet();
+      return await createABlankSheetWithCustomizedName();
     default:
       return res.status(405).end(`Not allowed. Received ${req.method}`);
   }
@@ -70,16 +36,35 @@ const SpreadsheetsHandler = async (req, res) => {
     }
   }
 
-  async function createABlankSheet() {
+  async function createABlankSheetWithCustomizedName() {
     console.log('running createABlankSheet');
 
     const { body } = req;
 
-    // console.log(body);
+    const parsedBody = JSON.parse(body);
 
-    // const parsedBody = JSON.parse(body);
+    console.log('request body sent to the server via api: ', parsedBody);
 
-    // console.log('the parsed request body is: ', parsedBody);
+    const token = await getNextAuthToken(req);
+    const oauth2Client = await authenticate(token);
+    const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
+
+    try {
+      const response = await sheets.spreadsheets.create({
+        requestBody: {
+          properties: {
+            title: parsedBody.title,
+          },
+        },
+      });
+
+      console.log('create spreadsheet response: ', response);
+      res.send('spreadsheet created and the response is: ', response);
+
+    } catch (err) {
+      console.error(err);
+    }
+
   }
 
 };
