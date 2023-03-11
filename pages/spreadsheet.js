e8896085a4d53d218dc20728da3bbf753e07a2f6
Rui Hu <hurui141@gmail.com>
2023-03-11 00:18:58 -0700

    Implement/Improve redirect using next/router

--- pages/spreadsheet.js
+++ pages/spreadsheet.js
@@ -1,42 +1,47 @@
-import { useSession } from 'next-auth/react';
-
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { BigLogo } from '../src/components/BigLogo';
-import { Login } from '../src/components/Login';
-
-import { SpreadsheetForm } from '../src/components/SpreadsheetForm';
-
-const redirectToSpreadSheet = () => {
-  return window.location.assign(
-    `https://docs.google.com/spreadsheets/d/${process.env.NEXT_PUBLIC_SHEET_ID}`
-  );
-};
+import { ClickButton } from '../src/components/ClickButton';
 
 const Spreadsheet = () => {
-    const { data: session } = useSession();
 
-    return (
-      <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8'>
-        {session && (
-          <>
-            <BigLogo
-              iconName='table'
-              heading='Redirecting'
-              subheading='to your spreadsheet'
-            />
-          </>
-        )}
-        {!session && (
-          <>
-            <BigLogo
-              iconName='table'
-              heading='Sign in required'
-              subheading='Please sign in to view your spreadsheet'
-            />
-            <Login message='Sign in now' />
-          </>
-        )}
-      </div>
-    );
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const redirectToSpreadSheet = () => {
+    router.push(`https://docs.google.com/spreadsheets/d/${process.env.NEXT_PUBLIC_SHEET_ID}`);
+  }
+
+  return (
+    <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8'>
+      {session && (
+        <>
+          <BigLogo
+            iconName='table'
+            heading='Click to Navigate'
+            subheading='to the spreadsheet'
+          />
+          <ClickButton
+            message='Navigate to Spreadsheet'
+            actionFn={redirectToSpreadSheet}
+          />
+        </>
+      )}
+      {!session && (
+        <>
+          <BigLogo
+            iconName='table'
+            heading='Sign in required'
+            subheading='Please sign in to view your spreadsheet'
+          />
+          <ClickButton
+            message='Sign in now'
+            actionFn={signIn}
+          />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Spreadsheet;
